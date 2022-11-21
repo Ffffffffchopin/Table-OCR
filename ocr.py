1dce0861bcad9c315e4758086bb348a3127dce0b
xsy <1131585528@qq.com>
2022-11-21 23:03:25 +0800

    11/21 23:03

---
+++ ocr.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+ENLARGE=2
+window_name="Image"
+raw=cv2.imread(r"C:\Users\F.F.Chopin\Pictures\Screenshot_20220919_222336.jpg",1)
+height,width=raw.shape[:2]
+res=cv2.resize(raw,(width//ENLARGE,height//ENLARGE),interpolation=cv2.INTER_CUBIC)
+#print(type(raw))
+gray=cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+
+binary=cv2.adaptiveThreshold(~gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,35,-5)
+
+
+row,col=binary.shape
+scale=5
+kernel_row=cv2.getStructuringElement(cv2.MORPH_RECT,(col//scale,1))
+eroded_row=cv2.erode(binary,kernel_row,iterations=1)
+dilater_row=cv2.dilate(eroded_row,kernel_row,iterations=1)
+
+kernel_col=cv2.getStructuringElement(cv2.MORPH_RECT,(1,row//scale))
+eroded_col=cv2.erode(binary,kernel_col,iterations=1)
+dilater_col=cv2.dilate(eroded_col,kernel_col,iterations=1)
+
+#ss_row=np.hstack((dilater_row,binary))
+ss_col=np.hstack((dilater_col,binary))
+#ss=np.hstack((binary,cv2.add(dilater_col,dilater_row)))
+cv2.imshow(window_name,np.hstack((eroded_row,dilater_row)))
+
+
+
+cv2.waitKey()
+
+#import cv2
+
+
